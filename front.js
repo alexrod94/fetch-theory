63c9eaaa72a0ddce840e29e21e4d2ff7fab903cf
alexrod94 <alexrod94@gmail.com>
2022-07-09 11:38:13 +0200

    Add initial files

---
+++ front.js
@@ -0,0 +1,64 @@
+const button = document.querySelector("button");
+const create = document.querySelector("#create");
+const posts = document.querySelector(".posts");
+
+button.addEventListener("click", getPosts);
+create.addEventListener("click", createPost);
+
+async function getPosts() {
+  const res = await fetch("http://localhost:3000/");
+  const finalRes = await res.json();
+  finalRes.forEach(
+    (el) =>
+      (posts.innerHTML += `<h2>${el.title}</h2>
+  <p>${el.shortDesc}</p>`)
+  );
+}
+
+async function createPost() {
+  const res = await fetch("http://localhost:3000/", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: {
+      title: "Título nuevo",
+      description: "Esta es mi descripción",
+    },
+  });
+  const finalRes = await res.json();
+  console.log(finalRes);
+}
+
+async function updatePost() {
+  const res = await fetch("http://localhost:3000/1", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "PUT",
+    body: {
+      title: "Título nuevo",
+      description: "Esta es mi descripción",
+    },
+  });
+  const finalRes = await res.json();
+  console.log(finalRes);
+}
+
+async function createPost() {
+  const res = await fetch("http://localhost:3000/1", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "DELETE",
+  });
+  const finalRes = await res.json();
+  console.log(finalRes);
+}
+
+const myJson = {
+  id: 1,
+  title: "asdfasdfasd",
+};
+
+const array = [];
